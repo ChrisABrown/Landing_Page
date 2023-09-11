43ce073d7b2271dd65acadaf769d308b3a546e22
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-09-10 22:22:24 -0500

    continuing to add helper functions to make set up easier, createTabs, addSection, buildNav

--- js/app.js
+++ js/app.js
@@ -22,12 +22,41 @@
  * Define Global Variables
  *
  */
+const ACTIVE_CLASS = 'your-active-class'
+let isActive = false
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+function createTabs(num) {
+  for (let i = 1; i <= num; i++) {
+    let item = document.createElement('li')
+    item.className = 'menu__link'
+  }
+  let tabs = document.querySelectorAll('.menu__link')
+  return tabs
+}
+
+function addSection(num) {}
+
+function buildNav() {
+  let nav = document.querySelector('#navbar__list')
+  let sections = document.querySelectorAll('.landing__container')
+  let tabs = createTabs(sections.length)
+  tabs.forEach((element) => {
+    nav.appendChild(element)
+  })
+}
+
+function switchClass() {
+  let options = document.getElementsByTagName('section').classList
+  if (options.include(ACTIVE_CLASS)) {
+    isActive = true
+  }
+  isActive = false
+}
 
 /**
  * End Helper Functions
@@ -36,7 +65,7 @@
  */
 
 // build the nav
-
+document.addEventListener('DOMContentLoaded', createTabs())
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
