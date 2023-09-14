f86327f36b870fb84717e55f647c9479f3fcda00
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-09-13 22:38:12 -0500

    changed css to display tabs as horizontal bar, looking into IntersectionObserver, taking a break for the night

--- js/app.js
+++ js/app.js
@@ -24,34 +24,42 @@
  */
 const ACTIVE_CLASS = 'your-active-class'
 let isActive = false
+const sectionBuild = `<div class='landing__container'><h2>New Section</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quamnunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duislectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi atincidunt felis. Sed leo nunc, pharetra et elementum non, faucibusvitae elit. Integer nec libero venenatis libero ultricies molestiesemper in tellus. Sed congue et odio sed euismod.</p><p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinargravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, egetelementum tortor mollis non. </p></div>`
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
-function createTabs(num) {
-  for (let i = 1; i <= num; i++) {
+//populate the tabs with dataset from the sections then returning an array holding the completed tabs
+function createTabs() {
+  let sections = document.querySelectorAll('section')
+  let tabs = []
+  for (let i = 0; i < sections.length; i++) {
     let item = document.createElement('li')
     item.className = 'menu__link'
+    item.textContent = sections[i].dataset.nav
+    tabs.push(item)
   }
-  let tabs = document.querySelectorAll('.menu__link')
   return tabs
 }
 
+//Not very dynamic for adding a section with different html or anything else but it gets the job done
 function addSection(num) {
   let lastChild = document.getElementById('section3')
-  let sections = document.getElementsByTagName('section')
-  for (let i = num; i < num + sections.length; i++) {
+  for (let i = 0; i < num; i++) {
     let section = document.createElement('section')
-    section.innerHTML = `<div class='landing__container'><h2>Section ${i}</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quamnunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duislectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi atincidunt felis. Sed leo nunc, pharetra et elementum non, faucibusvitae elit. Integer nec libero venenatis libero ultricies molestiesemper in tellus. Sed congue et odio sed euismod.</p><p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinargravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, egetelementum tortor mollis non. </p></div>`
+    section.innerHTML = sectionBuild
+    section.setAttribute('id', 'newSection')
+    section.setAttribute('data-nav', 'New Section')
     lastChild.insertAdjacentElement('afterend', section)
   }
 }
 
+//Uses createTab functions returned array of tabs to then add tabs to the page
 function buildNav() {
   let nav = document.querySelector('#navbar__list')
-  let sections = document.querySelectorAll('.landing__container')
+  let sections = document.querySelectorAll('section')
   let tabs = createTabs(sections.length)
   tabs.forEach((element) => {
     nav.appendChild(element)
@@ -59,6 +67,7 @@ function buildNav() {
 }
 
 function switchClass() {
+  let observer = new IntersectionObserver()
   let options = document.getElementsByTagName('section').classList
   if (options.include(ACTIVE_CLASS)) {
     isActive = true
@@ -73,8 +82,8 @@ function switchClass() {
  */
 
 // build the nav
-document.addEventListener('DOMContentLoaded', addSection(2))
-document.addEventListener('DOMContentLoaded', createTabs(5))
+document.addEventListener('DOMContentLoaded', addSection(1))
+document.addEventListener('DOMContentLoaded', createTabs())
 document.addEventListener('DOMContentLoaded', buildNav())
 // Add class 'active' to section when near top of viewport
 
