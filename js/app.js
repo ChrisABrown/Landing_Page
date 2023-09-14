31e6d1ffc1291d3ba0396ec3ce8c9befdd04d8fb
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-09-13 20:28:00 -0500

    trying to get the dynamic ativity of section adding to work

--- js/app.js
+++ js/app.js
@@ -41,10 +41,10 @@ function createTabs(num) {
 
 function addSection(num) {
   let lastChild = document.getElementById('section3')
-  // let sections = document.getElementsByTagName('section')
-  for (let i = 0; i < num; i++) {
+  let sections = document.getElementsByTagName('section')
+  for (let i = num; i < num + sections.length; i++) {
     let section = document.createElement('section')
-    section.innerHTML = `<div class='landing__container'><h2>Section ${i}</h2></div>`
+    section.innerHTML = `<div class='landing__container'><h2>Section ${i}</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quamnunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duislectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi atincidunt felis. Sed leo nunc, pharetra et elementum non, faucibusvitae elit. Integer nec libero venenatis libero ultricies molestiesemper in tellus. Sed congue et odio sed euismod.</p><p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinargravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, egetelementum tortor mollis non. </p></div>`
     lastChild.insertAdjacentElement('afterend', section)
   }
 }
